0ea420caee2ae5fdac7b6b491af2461ad4527be0
Nicholas J. Poet <nicholasjpoet@gmail.com>
2026-06-05 18:40:55 -0700

    update config for static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://npoet.dev',
-	integrations: [mdx(), sitemap(), react()],
+  site: 'https://npoet.dev',
+  output: 'static',
+  integrations: [
+    mdx(),
+    react(),
+    sitemap(),
+  ],
 });
